4202d01dfed60f0ec79c3d337ac0b3f0c3f2818a
Yesid Rincon <dairo.rincon@uptc.edu.co>
2024-04-24 18:28:28 -0500

    update table with totals

--- src/components/TableDelivery.jsx
+++ src/components/TableDelivery.jsx
@@ -5,7 +5,7 @@ import RegisterDelivery from './RegisterDelivery';
 const TableDelivery = () => {
     const user_id = "661fdfe5ce2a67a15ead2115";
     const [dataCustomer, setDataCustomer] = useState();
-    const [selectedCustomer, setSelectedCustomer] = useState()
+    const [selectedCustomer, setSelectedCustomer] = useState(null);
 
     useEffect(() => {
         const getCustomers = async () => {
@@ -28,17 +28,39 @@ const TableDelivery = () => {
         }
         return '0';
     };
-    const handleOpenModal = async (_id) =>{
+
+    const calculateTotalBottles = (customer) => {
+        let total = 0;
+        if (customer.deliveries) {
+            customer.deliveries.forEach(delivery => {
+                total += delivery.quantity;
+            });
+        }
+        return total;
+    };
+
+    const calculateTotalBottlesByDay = (index) => {
+        let total = 0;
+        if (dataCustomer) {
+            dataCustomer.forEach(customer => {
+                total += parseInt(getDeliveryQuantity(customer, index), 10);
+            });
+        }
+        return total;
+    };
+
+    const handleOpenModal = async (_id) => {
         try {
-            const response = await fetch(`${BASE_URL}/customer/getCustomer/${_id}`)
-            const data = await response.json()
-            setSelectedCustomer(data.data)
+            const response = await fetch(`${BASE_URL}/customer/getCustomer/${_id}`);
+            const data = await response.json();
+            setSelectedCustomer(data.data);
         } catch (error) {
-            
+            console.error('Error fetching customer data:', error);
         }
     }
-    const handleCloseModal = () =>{
-        setSelectedCustomer(null)
+
+    const handleCloseModal = () => {
+        setSelectedCustomer(null);
     }
 
     return (
@@ -48,7 +70,6 @@ const TableDelivery = () => {
                     <tr>
                         <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Cliente</th>
                         <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Acciones</th>
-
                         {[...Array(7)].map((_, index) => (
                             <th key={index} className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Día {index + 1}</th>
                         ))}
@@ -62,34 +83,53 @@ const TableDelivery = () => {
                         dataCustomer.map((customer, index) => (
                             <tr key={index} className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}>
                                 <td className="px-6 py-4 whitespace-nowrap">{customer.customer.name}</td>
-                                <td> <button class="bg-blue-500 hover:bg-blue-700 text-white justify-center rounded" onClick={()=>{handleOpenModal(customer.customer._id)}}>
-                                    <svg
-                                        className="w-6 h-6 mr-2"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                                        ></path>
-                                    </svg>
-                                </button></td>
-
+                                <td>
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white justify-center rounded" onClick={() => { handleOpenModal(customer.customer._id) }}>
+                                        <svg
+                                            className="w-6 h-6 mr-2"
+                                            fill="none"
+                                            stroke="currentColor"
+                                            viewBox="0 0 24 24"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth="2"
+                                                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                                            ></path>
+                                        </svg>
+                                    </button>
+                                </td>
                                 {[...Array(7)].map((_, index) => (
                                     <td key={index} className="px-6 py-4 whitespace-nowrap">{getDeliveryQuantity(customer, index)}</td>
                                 ))}
-                                <td className="px-6 py-4 whitespace-nowrap">${customer.customer.price_per_bottle}</td>
-                                <td className='px-6 py-4 whitespace-nowra'>0</td>
-                                <td className='px-6 py-4 whitespace-nowra'>0</td>
+                                <td className="px-6 py-4 whitespace-nowrap">{customer.customer.price_per_bottle.toLocaleString('es-CO', {
+                                        style: 'currency',
+                                        currency: 'COP',
+                                    })}</td>
+                                <td className='px-6 py-4 whitespace-nowrap'>{calculateTotalBottles(customer)}</td>
+                                <td className='px-6 py-4 whitespace-nowrap'>
+                                    {(calculateTotalBottles(customer) * customer.customer.price_per_bottle).toLocaleString('es-CO', {
+                                        style: 'currency',
+                                        currency: 'COP',
+                                    })}
+                                </td>
                             </tr>
                         ))
                     ) : (
                         <tr><td colSpan="9" className="px-6 py-4 text-center">No hay datos</td></tr>
                     )}
+                    
+                    <tr>
+                        <td className="px-6 py-4 whitespace-nowrap">Total botellas por día:</td>
+                        <td ><span></span></td>
+                        <td colSpan="8">
+                            {[...Array(7)].map((_, index) => (
+                                <span key={index} className="px-6 py-4 whitespace-nowrap">{calculateTotalBottlesByDay(index)}</span>
+                            ))}
+                        </td>
+                    </tr>
                 </tbody>
             </table>
             {selectedCustomer && <RegisterDelivery customer={selectedCustomer} onClose={handleCloseModal} />}
